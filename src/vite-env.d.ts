835496e226ac67713049550c8d953500e8835d6d
lan2557 <1215758259@qq.com>
2024-04-23 16:52:56 +0800

    创建基础配置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+export interface IElectronAPI {
+    setTitle: (value) => Promise<void>,
+}
+
+declare global {
+    interface Window {
+        electronAPI: IElectronAPI,
+    }
+}

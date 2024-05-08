9e0108958e1e18f62c99af95c41522e46f4afe26
lan2557 <1215758259@qq.com>
2024-05-08 09:40:12 +0800

    完善了friendsLists界面

--- src/components/friends/friendsLists/friendsLists.ts
+++ src/components/friends/friendsLists/friendsLists.ts
@@ -3,9 +3,21 @@ import {ref} from "vue";
 export const friendsList = () => {
     const friends_lists = ref(true);
     const friend_signIn_status = ref(true);
+    const gender = ref("man");
+    const remark = ref("朋友备注(最大10字)");
+    const warn_show = ref(false);
+
+    // 判断备注长度并上传
+    const changeRemark = () => {
+        warn_show.value = remark.value.length > 12;
+    }
 
     return {
         friends_lists,
         friend_signIn_status,
+        gender,
+        remark,
+        changeRemark,
+        warn_show,
     }
 }
--- src/components/friends/friendsLists/friendsLists.vue
+++ src/components/friends/friendsLists/friendsLists.vue
@@ -20,12 +20,34 @@
       <div class="dividing_line"/>
       <div class="friend_private_information">
         <header>
-          <el-icon size="20">
-            <Male style="color:#4B70E2;"/>
-            <Female style="color: pink"/>
-          </el-icon>
-          <p>男</p>
+          <div class="gender">
+            <el-icon size="20">
+              <Male style="color:#4B70E2;" v-if="gender === 'man'"/>
+              <Female style="color: #ffb9c6" v-else/>
+            </el-icon>
+            <p v-if="gender === 'man'">男</p>
+            <p v-else>女</p>
+          </div>
+          <p class="age">22岁</p>
+          <p class="constellation">11月24日 <span>射手座</span></p>
         </header>
+        <div class="friend_information">
+          <div class="remark">
+            <el-icon size="20">
+              <Edit/>
+            </el-icon>
+            <p>备注</p>
+            <p class="warn" v-show="warn_show">备注不超过10字</p>
+            <input type="text" contenteditable="true" v-model="remark" @change="changeRemark">
+          </div>
+          <div class="signature">
+            <el-icon size="20">
+              <EditPen/>
+            </el-icon>
+            <p>签名</p>
+            <p class="friend_signature">朋友签名(最大20字)</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -33,11 +55,15 @@
 
 <script setup lang="ts">
 import {friendsList} from "./friendsLists.ts";
-import {Male, Female} from "@element-plus/icons-vue";
+import {Male, Female, EditPen, Edit} from "@element-plus/icons-vue";
 
 const {
   friends_lists,
   friend_signIn_status,
+  gender,
+  remark,
+  changeRemark,
+  warn_show,
 } = friendsList();
 </script>
 

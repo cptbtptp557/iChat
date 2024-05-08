dae64fbcf571fd00c568bd6991c74134a2a0ebdb
lan2557 <1215758259@qq.com>
2024-05-08 21:37:54 +0800

    完善添加好友、创建群聊功能

--- src/components/chat/chatFriendsBar/homeFriendsBar.vue
+++ src/components/chat/chatFriendsBar/homeFriendsBar.vue
@@ -7,100 +7,124 @@
         </el-icon>
         <input type="text" placeholder="搜索">
       </label>
-      <el-button class="add">
-        <el-icon>
-          <Plus/>
-        </el-icon>
-      </el-button>
+      <el-popover
+          placement="bottom"
+          :width="150"
+          trigger="click"
+          content="this is content, this is content, this is content"
+      >
+        <el-button style="position: relative;left: 11px;margin-bottom: 10px">
+          <el-icon>
+            <DataAnalysis/>
+          </el-icon>
+          <p>发起群聊</p>
+        </el-button>
+        <el-button>
+          <el-icon>
+            <User/>
+          </el-icon>
+          <p>添加好友</p>
+        </el-button>
+        <template #reference>
+          <el-button class="add">
+            <el-icon>
+              <Plus/>
+            </el-icon>
+          </el-button>
+        </template>
+      </el-popover>
     </header>
-      <div class="chat_friends">
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>今天</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-            <el-badge :value="10" :max="99" class="el-badge"/>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
-        </div>
-        <div class="friends">
-          <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-          <div class="information">
-            <p>接收方用户</p>
-            <p>2024/01/28</p>
-            <p>接收的消息asdsdasdsdasdasdsadsd</p>
-          </div>
+    <div class="chat_friends">
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>今天</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+          <el-badge :value="10" :max="99" class="el-badge"/>
         </div>
       </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+      <div class="friends">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="information">
+          <p>接收方用户</p>
+          <p>2024/01/28</p>
+          <p>接收的消息asdsdasdsdasdasdsadsd</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {Plus, Search} from "@element-plus/icons-vue";
+import {DataAnalysis, Plus, Search, User} from "@element-plus/icons-vue";
+import {homeFriendsBar} from "./homeFriendsBar.ts";
+
+const {} = homeFriendsBar();
 </script>
 
 <style scoped>
--- src/components/friends/allFriends/allFriends.vue
+++ src/components/friends/allFriends/allFriends.vue
@@ -1,163 +1,206 @@
 <template>
   <div class="allFriendsBody">
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="true">
-            <div></div>
-            <p>在线</p>
+    <header>
+      <el-icon class="search">
+        <Search/>
+      </el-icon>
+      <input type="text" placeholder="搜索">
+    </header>
+    <main>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名朋友个性签名朋友个性签名朋友个性签名朋友个性签名朋友个性签名朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="!true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="!true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="!true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="!true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="!true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="!true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="!true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
-    <div class="friends_introduce">
-      <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
-      <div class="friends_text_introduce">
-        <p>朋友昵称</p>
-        <div class="SignIn_status">
-          <div class="online" v-if="!true">
-            <div></div>
-            <p>在线</p>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="!true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
           </div>
-          <div class="offline" v-else>
-            <div></div>
-            <p>离线</p>
-          </div>
+          <p>朋友个性签名</p>
         </div>
-        <p>朋友个性签名</p>
       </div>
-    </div>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
+          </div>
+          <p>朋友个性签名</p>
+        </div>
+      </div>
+      <div class="friends_introduce">
+        <img src="../../../../public/chat-avatar/from-user.png" alt="朋友头像">
+        <div class="friends_text_introduce">
+          <p>朋友昵称</p>
+          <div class="SignIn_status">
+            <div class="online" v-if="!true">
+              <div></div>
+              <p>在线</p>
+            </div>
+            <div class="offline" v-else>
+              <div></div>
+              <p>离线</p>
+            </div>
+          </div>
+          <p>朋友个性签名</p>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
 import {allFriends} from "./allFriends.ts";
+import {Search} from "@element-plus/icons-vue";
 
 const {} = allFriends();
 </script>

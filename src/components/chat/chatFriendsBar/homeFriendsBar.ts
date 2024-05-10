673f42fe72002d3845d1b350b078c2241c32aaac
lan2557 <1215758259@qq.com>
2024-05-10 19:28:40 +0800

    完善添加好友、创建群聊功能

--- src/components/chat/chatFriendsBar/homeFriendsBar.ts
+++ src/components/chat/chatFriendsBar/homeFriendsBar.ts
@@ -1,5 +1,36 @@
-export const homeFriendsBar = () => {
-    return {
+import {ref} from "vue";
 
+export const homeFriendsBar = () => {
+    const add_friend = ref(false); // 添加好友框
+    const find_logotype = ref(); // 添加好友内输入的内容
+
+    const find = () => {
+        console.log(find_logotype.value);
+    }
+
+    const empty = () => {
+        find_logotype.value = "";
+    }
+
+    const changeBorder = (even: any) => {
+        const user = document.getElementById("user") as HTMLElement
+        const group_chats = document.getElementById("group_chats") as HTMLElement;
+        console.log(even.srcElement.__vnode.children )
+        if (even.srcElement.__vnode.children === "用户") {
+            user.style.borderColor = "#000";
+            group_chats.style.borderColor = "transparent";
+        }
+        if(even.srcElement.__vnode.children === "群聊") {
+            user.style.borderColor = "transparent";
+            group_chats.style.borderColor = "#000";
+        }
+    }
+
+    return {
+        add_friend,
+        find,
+        find_logotype,
+        empty,
+        changeBorder,
     }
 }
--- src/components/chat/chatFriendsBar/homeFriendsBar.vue
+++ src/components/chat/chatFriendsBar/homeFriendsBar.vue
@@ -8,19 +8,18 @@
         <input type="text" placeholder="搜索">
       </label>
       <el-popover
-          placement="bottom"
+          placement="bottom-start"
           :width="150"
           trigger="click"
-          content="this is content, this is content, this is content"
       >
         <el-button style="position: relative;left: 11px;margin-bottom: 10px">
-          <el-icon>
+          <el-icon style="position: relative; left: -5px">
             <DataAnalysis/>
           </el-icon>
           <p>发起群聊</p>
         </el-button>
-        <el-button>
-          <el-icon>
+        <el-button @click="add_friend = true">
+          <el-icon style="position: relative; left: -5px">
             <User/>
           </el-icon>
           <p>添加好友</p>
@@ -33,6 +32,169 @@
           </el-button>
         </template>
       </el-popover>
+      <el-dialog
+          v-model="add_friend"
+          width="600"
+          draggable
+          style="border-radius: 5px"
+      >
+        <template #header>
+          <p style="text-align: center;letter-spacing: 2px;margin-left: 40px">搜索</p>
+        </template>
+        <template #footer>
+          <div class="add_input">
+            <el-icon>
+              <Search/>
+            </el-icon>
+            <input type="text" placeholder="输入搜索关键词..." v-model="find_logotype" @input="find">
+            <el-icon @click="empty">
+              <CircleClose/>
+            </el-icon>
+          </div>
+          <div class="inquire_classify">
+            <p @click="changeBorder" id="user">用户</p>
+            <p @click="changeBorder" id="group_chats">群聊</p>
+          </div>
+          <div class="search_results">
+            <div v-if="find_logotype">
+              <el-icon size="100">
+                <MessageBox/>
+              </el-icon>
+              <h2>输入搜索关键词</h2>
+            </div>
+            <div class="show_search_outcome" v-else>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+              <div class="search_user">
+                <img src="../../../../public/chat-avatar/from-user.png" alt="用户头像">
+                <div>
+                  <p>昵称</p>
+                  <p>iId</p>
+                  <p>个性签名</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </el-dialog>
     </header>
     <div class="chat_friends">
       <div class="friends">
@@ -121,10 +283,16 @@
 </template>
 
 <script setup lang="ts">
-import {DataAnalysis, Plus, Search, User} from "@element-plus/icons-vue";
+import {CircleClose, DataAnalysis, MessageBox, Plus, Search, User} from "@element-plus/icons-vue";
 import {homeFriendsBar} from "./homeFriendsBar.ts";
 
-const {} = homeFriendsBar();
+const {
+  add_friend,
+  find,
+  find_logotype,
+  empty,
+  changeBorder,
+} = homeFriendsBar();
 </script>
 
 <style scoped>

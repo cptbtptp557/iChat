3f79389235d61cc964991220367a011ee2df050f
lan2557 <1215758259@qq.com>
2024-05-11 15:27:58 +0800

    完善添加好友、创建群聊功能

--- src/components/chat/chatFriendsBar/homeFriendsBar.ts
+++ src/components/chat/chatFriendsBar/homeFriendsBar.ts
@@ -17,12 +17,16 @@ export const homeFriendsBar = () => {
         const group_chats = document.getElementById("group_chats") as HTMLElement;
         console.log(even.srcElement.__vnode.children )
         if (even.srcElement.__vnode.children === "用户") {
-            user.style.borderColor = "#000";
+            user.style.borderColor = "#4B70E2FF";
+            user.style.color = "#4B70E2FF";
             group_chats.style.borderColor = "transparent";
+            group_chats.style.color = "#000";
         }
         if(even.srcElement.__vnode.children === "群聊") {
             user.style.borderColor = "transparent";
-            group_chats.style.borderColor = "#000";
+            user.style.color = "#000";
+            group_chats.style.borderColor = "#4B70E2FF";
+            group_chats.style.color = "#4B70E2FF";
         }
     }
 
